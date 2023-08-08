4c78fb67b445e8accc6efd3976b2e5814734af92
vasanth064 <vasanthvdev@gmail.com>
2023-08-08 23:35:06 +0530

    added about

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -4,6 +4,34 @@ const About = () => {
   return (
     <section id='form'>
       <h2>About </h2>
+      <p>
+        Welcome to our cutting-edge Credential Intelligence platform, a
+        revolutionary tool designed to bring you a step closer to the future of
+        data visualization and analysis. Our platform provides users with a
+        unique opportunity to explore connections, relationships, and insights
+        hidden within diverse sets of credentials, such as phone numbers,
+        usernames, and email addresses.
+      </p>
+      <h3>Our Mission</h3>
+      <p>
+        At the heart of our mission is the commitment to empower individuals and
+        organizations with the ability to harness the power of data for informed
+        decision-making. In an increasingly interconnected world, understanding
+        the relationships between different pieces of information is essential
+        for safeguarding privacy, enhancing security, and discovering patterns
+        that drive meaningful outcomes.
+      </p>
+      <h3>Unveiling Hidden Connections</h3>
+      <p>
+        Imagine a world where you can enter a single credential – a phone
+        number, a username, or an email address – and instantly unveil a web of
+        connections that extend beyond the surface. Our platform's sophisticated
+        algorithms crawl through vast datasets, both public and aggregated from
+        trusted sources, to map out associations that would otherwise remain
+        hidden from view. What emerges is a comprehensive graph that showcases
+        relationships, interactions, and possible correlations.
+      </p>
+      <p>Made with ❤️ @iQube</p>
     </section>
   );
 };
